c772052e1651aba578646e11b3eef3ccd8c2048b
UgoRastell <urastell@normandiewebschool.fr>
2022-10-11 16:10:55 +0200

    organisation des fichier

--- index.php
+++ index.php
@@ -4,29 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="media/favicon_nws.ico">
+    <title>Accueil annuaire NWS</title>
 </head>
 <body>
-  <section class="logo">
-    <img src="./media/logo_nws.svg" alt="">
-  </section>
+  <div class="logo">
+    <img src="./assets/media/logo_nws.svg" alt="">
+</div>
 
-  <section class="input_add">
-    <form method="post" action="" enctype="multipart/form-data">
-        <label>Prenom</label>
-        <input type="text" name="prenom">
-        <label>Nom</label>
-        <input type="text" name="nom">
-        <label>Mail</label>
-        <input type="text" name="mail">
-        <label>Numéro de téléphone</label>
-        <input type="text" name="numero">
-        <input type="submit" value="Ajouter" name="btn-ajouter"/>
-        <input type="submit" value="Annuaire" name="btn-annuaire" class="btn-annuaire"/>
-        
-    </form>
-  </section>
-
+  <div class="result">
+       <div class="result-content">
+            <h3>ANNUAIRE</h3>
+            <a href="">AJOUTER</a>
+            <div class="liste-personne">
+              <div class="personne">
+                <div class="text">
+                  <strong><p class="nomPrenom"></p>Ugo Rastell</strong>
+                  <p class="email">fzfzfzf@fsfsfsfsf.com</p>
+                  <p class="numeroTelephone">1651651565</p>
+                  <hr class="solid">
+                </div>
+              </div>
+            </div>
+        </div>
+  </div>
 </body>
 </html>
