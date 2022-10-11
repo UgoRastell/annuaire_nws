bb50771869ee941ebc94cf0dd9cc3aa0967c49de
UgoRastell <urastell@normandiewebschool.fr>
2022-10-11 16:11:43 +0200

    creation de class

---
+++ class/Annuaire.php
@@ -0,0 +1,36 @@
+<?php
+
+class Person
+{
+    public string $name;
+    public string $lastName;
+    public int $age;
+    public string $numeroTelephone;
+    public string $email;
+
+    public function __construct(string $prenom, string $nom, int $age_personne, string $numero_de_telephone, string $adresse_mail)
+    {
+        $this->name = $prenom;
+        $this->lastName = $nom;
+        $this->age = $age_personne;
+        $this->numeroTelephone = $numero_de_telephone;
+        $this->email = $adresse_mail;
+    }
+}
+
+class Student extends Person
+{
+    public string $speciality;
+    public string $last_diplome;
+
+    public function __construct(string $prenom, string $nom, int $age_personne, string $numero_de_telephone, string $adresse_mail, string $specialite, string $lastDiplome)
+    {
+        parent::__construct($prenom, $nom, $age_personne, $numero_de_telephone, $adresse_mail);
+        $this->speciality = $specialite;
+        $this->last_diplome = $lastDiplome;
+    }
+}
+
+$personne1 = new Student("ugo", "rastell", 19, "050505050", "zzgzgzgg<à@zzfzef.com", "developpement web", "bac sti2d");
+
+print_r($personne1);
